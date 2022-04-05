6f4d2c8e24637538b21f7a27f59ecad948c20aeb
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-04-05 15:04:30 -0700

    set up hello world with cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+// Preprocessor directive to include files in our program
+#include <iostream>
+
+// This is a comment!
+
+// Valid C++ programs MUST HAVE main(){}
+int main(){
+    // cout comes from <iostream> that allows us to print output to the screen
+    // std::cout prints the string, and it must be followed by <<
+    // ";" is used to end a statement, bit like JS
+    std::cout << "Hello World!";
+    // This is the "Exit Status" out of the program
+    return 0;
+}
